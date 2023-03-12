640efa38bd11985f314458e3c9a813c107b142fd
Jarryd Harris <mail@jarrydharris.org>
2023-03-12 13:26:55 +1100

    add terraform templates for azure vm and docker + nginx

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Reusable boilerplate code for personal python projects hosted on cloud.
 
 Currently only a setup script for Ubuntu 22.04.2 LTS
 
-To get started run the setup shell script
+To get started by installing terraform, poetry and docker, run the setup shell script.
 
 ```sh
 cd setup
@@ -15,5 +15,7 @@ sudo bash ./linux_setup.sh
 ```
 
 or install without the script from instructions
-https://developer.hashicorp.com/terraform/downloads
-https://python-poetry.org/docs/#installing-with-the-official-installer
+
+- [Terraform](https://developer.hashicorp.com/terraform/downloads)
+- [Poetry](https://python-poetry.org/docs/#installing-with-the-official-installer)
+- [Docker Engine](https://docs.docker.com/engine/install/ubuntu/#set-up-the-repository)
--- setup/linux_setup.sh
+++ setup/linux_setup.sh
@@ -19,3 +19,18 @@ curl -sSL https://install.python-poetry.org | python3 -
 # Get the users home directory and then export poetry path
 homedir=$( getent passwd "$USER" | cut -d: -f6 )
 export PATH="${homedir}/.local/bin:$PATH"
+
+# Install docker
+apt-get update
+apt-get install ca-certificates curl gnupg lsb-release
+
+mkdir -m 0755 -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+apt-get update
+
+apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
