dd4414d7b085d8fb45d4bb99633f00b56dcf307d
Jarryd Harris <mail@jarrydharris.org>
2023-03-16 20:50:01 +1100

    set up reverse proxy and docker compose

--- src/app/appEntry.py
+++ src/app/appEntry.py
@@ -1,6 +1,7 @@
 import os
 
 from flask import Flask, render_template
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 
 def create_app(test_config=None):
@@ -10,6 +11,8 @@ def create_app(test_config=None):
         # a default secret that should be overridden by instance config
         SECRET_KEY="dev"
     )
+
+    app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1)
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
